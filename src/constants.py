d4ad4e4b3746da3dd52e5b180454962f559056d6
Jonathan Laster <1691508+last-dev@users.noreply.github.com>
2024-04-03 13:52:22 -0400

    Remove unused fields

--- src/constants.py
+++ src/constants.py
@@ -4,7 +4,6 @@ from enum import Enum
 
 
 class Stage(Enum):
-    Dev = 'Dev'
     Prod = 'Prod'
     Beta = 'Beta'
 
@@ -18,8 +17,6 @@ class AwsAccount:
     account_id: str
     stage: Stage
     region: AwsRegion
-    disambiguator: str
-    api_gw_endpoint_ssm_param_name: str
 
 
 # Define my development accounts for each stage
@@ -29,13 +26,9 @@ class Accounts:
         account_id=os.environ['SPOTIFICITY_BETA_ACCT'],
         stage=Stage.Beta,
         region=AwsRegion.IAD,
-        disambiguator=f'spotificity',
-        api_gw_endpoint_ssm_param_name='/Spotificity/ApiGatewayEndpointUrl/beta',
     )
     prod: AwsAccount = AwsAccount(
         account_id=os.environ['SPOTIFICITY_PROD_ACCT'],
         stage=Stage.Prod,
         region=AwsRegion.IAD,
-        disambiguator=f'spotificity',
-        api_gw_endpoint_ssm_param_name='/Spotificity/ApiGatewayEndpointUrl/prod',
     )
