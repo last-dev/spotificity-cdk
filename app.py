0b10feb51a3163154bfb8f7f78e0a461e49c186c
Jonathan <jonathan@laster.dev>
2024-04-03 05:25:25 -0400

    Teensy cleanup

--- app.py
+++ app.py
@@ -8,11 +8,7 @@ from src.stacks.database_stack import DatabaseStack
 app = App()
 
 database_stack = DatabaseStack(app, 'DatabaseStack')
-backend_stack = BackendStack(
-    app,
-    'BackendStack',
-    monitored_artist_table=database_stack.artist_table,
-)
+backend_stack = BackendStack(app, 'BackendStack', artist_table=database_stack.artist_table)
 backend_stack.add_dependency(database_stack)
 
 app.synth()
